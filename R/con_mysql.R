e8046b95f86e9b756a3e5213bfaf99f909b937df
JARS3N <JARS3N@users.noreply.github.com>
2017-10-03 12:58:16 -0400

    Create con_mysql.R

---
+++ R/con_mysql.R
@@ -0,0 +1,10 @@
+con_mysql<-function(){
+requrire(RMySQL)
+ConnectInfo<-sharpen(triton())
+dbConnect(RMySQL::MySQL(),
+                     dbname=ConnectInfo[1],
+                     user=ConnectInfo[2],
+                     password=ConnectInfo[3],
+                     host=ConnectInfo[4],
+                     port=as.numeric(ConnectInfo[5]))
+}
