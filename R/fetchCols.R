8940501913bb9763191a7f85ef8f7047dc924dd3
JARS3N <JARS3N@users.noreply.github.com>
2017-10-03 13:04:39 -0400

    Update fetchCols.R

--- R/fetchCols.R
+++ R/fetchCols.R
@@ -1,13 +1,6 @@
 fetchCols<-function(tbl,like=NULL){
   require(RMySQL)
-  ConnectInfo<-sharpen(triton())
-  my_db <- dbConnect(RMySQL::MySQL(),
-                     dbname=ConnectInfo[1],
-                     user=ConnectInfo[2],
-                     password=ConnectInfo[3],
-                     host=ConnectInfo[4],
-                     port=as.numeric(ConnectInfo[5]))
-
+  my_db <- con_mysql()
   if(is.null(like)){
     str<-paste0("show columns from ",tbl,";")
   }else{
