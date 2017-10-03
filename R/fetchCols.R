f8b37219195f41337eb3fedd799f8416c28451e5
JARS3N <JARS3N@users.noreply.github.com>
2017-10-03 12:28:42 -0400

    Update fetchCols.R

--- R/fetchCols.R
+++ R/fetchCols.R
@@ -1,6 +1,6 @@
 fetchCols<-function(tbl,like=NULL){
   require(RMySQL)
-  ConnectInfo<-DataStash::Triton()
+  ConnectInfo<-sharpen(triton())
   my_db <- dbConnect(RMySQL::MySQL(),
                      dbname=ConnectInfo[1],
                      user=ConnectInfo[2],
